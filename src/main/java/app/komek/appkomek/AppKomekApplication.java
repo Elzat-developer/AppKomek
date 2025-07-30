f6259b6c8b54f981e5e5a42f0170577eeb337b5f
Elzat-developer <fergenson.2020@gmail.com>
2025-07-30 11:29:19 +0500

    first commit

---
+++ src/main/java/app/komek/appkomek/AppKomekApplication.java
@@ -0,0 +1,13 @@
+package app.komek.appkomek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppKomekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppKomekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/komek/appkomek/AppKomekApplicationTests.java
@@ -0,0 +1,13 @@
+package app.komek.appkomek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppKomekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
